ed611f69a37d75330ab1d88918b06303fb15c2db
Von Christian <vc.halip@gmail.com>
2020-01-16 09:01:06 +0800

    update yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require('adminlte')
-
-
 require("chartkick")
 require("chart.js")
 require("trix")
